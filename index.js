600a98510cc026eb6cae3bb393d406230dfad59a
Pavel Klochkov <paul.klochkov@gmail.com>
2020-04-18 16:11:59 +0200

    First props

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const rootElement = document.querySelector('#app');
 
-const Hello = function () {
+const Hello = function ({time}) {
   return React.createElement('h1', {
     style: {
       color: 'orange'
     }
   },
-  'Hello from Netherlands and React')
+  'Hello from Netherlands and React \n' + time)
 }
 
-ReactDOM.render(React.createElement(Hello, {}, null), rootElement)
+ReactDOM.render(React.createElement(Hello, {time: new Date().toLocaleDateString()}, null), rootElement)
