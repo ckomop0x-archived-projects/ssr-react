a8c94ea596dbaceb84aa02e9e6c8720e7d21ce41
Pavel Klochkov <paul.klochkov@gmail.com>
2020-04-18 16:10:20 +0200

    Small fun with function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const rootElement = document.querySelector('#app');
 
-const myReactElement = React.createElement('h1', {
+const Hello = function () {
+  return React.createElement('h1', {
     style: {
       color: 'orange'
     }
   },
   'Hello from Netherlands and React')
+}
 
-ReactDOM.render(myReactElement, rootElement)
+ReactDOM.render(React.createElement(Hello, {}, null), rootElement)
