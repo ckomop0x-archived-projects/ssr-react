ce6e95e3edacc77481b75c8555f30ce4f1e58af6
Pavel Klochkov <paul.klochkov@gmail.com>
2020-04-18 16:22:03 +0200

    Convert to Class Component

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const rootElement = document.querySelector('#app');
 
-const Hello = function ({time}) {
-  return React.createElement('h1', {
-    style: {
-      color: 'orange'
-    }
-  },
-  'Hello from Netherlands and React \n' + time)
+class Hello extends React.Component {
+  state = {
+    time: new Date()
+  }
+
+  tick = () => {
+    this.setState(() => ({time: new Date()}))
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+
+  render() {
+    return (React.createElement('h1', {style: {color: 'orange'}}, 'Hello from Netherlands and React \n' + this.state.time))
+  }
 }
 
-ReactDOM.render(React.createElement(Hello, {time: new Date().toLocaleDateString()}, null), rootElement)
+ReactDOM.render(React.createElement(Hello, {}, null), rootElement)
